c5c56ee9cfdd1887517e28cbdb2d5d04cf0e40a5
Sean McDonald <mrseanmcd@gmail.com>
2016-03-18 19:40:08 -0400

    Testing compiler.

--- Hackathon/src/com/mcdonald/sean/Main.java
+++ Hackathon/src/com/mcdonald/sean/Main.java
@@ -1,10 +1,8 @@
 package com.mcdonald.sean;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Testing 123!");
 	}
-
 }
